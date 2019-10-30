f5e38b04361031117d6bae60e599642c227e41ce
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2019-10-30 16:16:22 -0500

    Live computing of gradient using degree range slider.

--- app/components/background-stage.js
+++ app/components/background-stage.js
@@ -2,8 +2,14 @@ import Component from '@ember/component';
 
 export default Component.extend({
 	message: 'Lets Play With Styles',
-	color1: '',
-	color2: '',
-	opacity: 1,
-	bgImage: '',
+	color1: 'white',
+	color2: 'black',
+	opacity: .95,
+	gradDegree: 17,
+	actions: {
+		updateGradDegree(newValueEvent) {
+			let newVal = newValueEvent.target.value;
+			this.set('gradDegree', newVal);
+		},
+	}	
 });
