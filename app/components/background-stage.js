441d423b9ed5b35fcfe1b8458868bcb6f5953c4a
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2019-10-30 16:40:26 -0500

    User can now edit opacity and message.

--- app/components/background-stage.js
+++ app/components/background-stage.js
@@ -3,13 +3,21 @@ import Component from '@ember/component';
 export default Component.extend({
 	message: 'Lets Play With Styles',
 	color1: 'white',
-	color2: 'black',
-	opacity: .95,
+	color2: 'red',
+	gradOpacity: .5,
 	gradDegree: 17,
 	actions: {
 		updateGradDegree(newValueEvent) {
 			let newVal = newValueEvent.target.value;
 			this.set('gradDegree', newVal);
+		},
+		updateGradOpacity(newValueEvent) {
+			let newVal = newValueEvent.target.value;
+			this.set('gradOpacity', newVal);
+		},
+		updateMessage(newValueEvent) {
+			let newVal = newValueEvent.target.value;
+			this.set('message', newVal);
 		},
 	}	
 });
